4eb450da5da5fa15c03aa259657fd83c197015f2
afokou <afokou@localhost>
2022-10-07 23:34:10 +0200

    add obstacles detection

--- src/application/Easy.java
+++ src/application/Easy.java
@@ -1,12 +1,16 @@
 package application;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable; 
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.shape.Polygon;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode; 
@@ -16,18 +20,53 @@ import javafx.scene.input.KeyEvent;
 public class Easy {
 	@FXML
 	ImageView character;
-    int imageX = 0;
-    int imageY = 0;
+	@FXML
+	Polygon obstacle1;
+	@FXML
+	Polygon obstacle2;
+	@FXML
+	Polygon obstacle3;
+	@FXML
+	Polygon obstacle4;
+	@FXML
+	Polygon obstacle5;
+	@FXML
+	Polygon obstacle6;
+	@FXML
+	Polygon obstacle7;
+	@FXML
+	Polygon obstacle8;
+	@FXML
+	Polygon obstacle9;
+	@FXML
+	Polygon obstacle10;
+	
+    double imageX = 0;
+    double imageY = 0;
 
 	public void initialise() {
+		List<Node> obstacles = new ArrayList<Node>();
+		obstacles.add(obstacle1);
+		obstacles.add(obstacle2);
+		obstacles.add(obstacle3);
+		obstacles.add(obstacle4);
+		obstacles.add(obstacle5);
+		obstacles.add(obstacle6);
+		obstacles.add(obstacle7);
+		obstacles.add(obstacle8);
+		obstacles.add(obstacle9);
+		obstacles.add(obstacle10);
+		
+		imageX = character.getLayoutX();
+		imageY = character.getLayoutY();
         character.getScene().addEventFilter(KeyEvent.ANY, key -> {
-            if (key.getCode().equals(KeyCode.RIGHT)) {
+            if (key.getCode().equals(KeyCode.RIGHT) && !isObstacleCollision(character, imageX + 5, imageY, obstacles)) {
                 imageX += 5;
-            } else if (key.getCode().equals(KeyCode.LEFT)) {
+            } else if (key.getCode().equals(KeyCode.LEFT) && !isObstacleCollision(character, imageX - 5, imageY, obstacles)) {
                 imageX -= 5;
-            } else if (key.getCode().equals(KeyCode.DOWN)) {
+            } else if (key.getCode().equals(KeyCode.DOWN) && !isObstacleCollision(character, imageX, imageY + 5, obstacles)) {
                 imageY += 5;
-            } else if (key.getCode().equals(KeyCode.UP)) {
+            } else if (key.getCode().equals(KeyCode.UP) && !isObstacleCollision(character, imageX, imageY - 5, obstacles)) {
                 imageY -= 5;
             }
             updateImageView();
@@ -37,7 +76,26 @@ public class Easy {
 	}
 	
     private void updateImageView() {
-        character.setX(imageX);
-        character.setY(imageY);
+        character.setLayoutX(imageX);
+        character.setLayoutY(imageY);
+    }
+    
+    public boolean isObstacleCollision(ImageView character, double imageX, double imageY, List<Node> obstacles)
+    {
+    	double oldImageX = character.getLayoutX();
+    	double oldImageY = character.getLayoutY();
+        character.setLayoutX(imageX);
+        character.setLayoutY(imageY);
+    	for (Node obstacle : obstacles) {
+    		if(obstacle != null && character.getBoundsInParent().intersects(obstacle.getBoundsInParent())) {
+    	        character.setLayoutX(oldImageX);
+    	        character.setLayoutY(oldImageY);
+	            return true;
+	        }
+    	}
+    	
+        character.setLayoutX(oldImageX);
+        character.setLayoutY(oldImageY);
+    	return false;
     }
 }
