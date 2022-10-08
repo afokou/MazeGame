2f3b286818ccb22c04a563f5294543f995a68056
Angeliki Fokou <afokou@Angelikis-MacBook-Pro.local>
2022-10-08 16:34:50 +0200

    Collect foods and images

--- src/application/Easy.java
+++ src/application/Easy.java
@@ -7,14 +7,15 @@ import java.util.ResourceBundle;
 
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.shape.Polygon;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode; 
-import javafx.scene.input.KeyEvent; 
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 
 public class Easy {
@@ -40,11 +41,44 @@ public class Easy {
 	Polygon obstacle9;
 	@FXML
 	Polygon obstacle10;
+	@FXML
+	ImageView image1;
+	@FXML
+	ImageView food1;
+	@FXML
+	ImageView inventoryFood1;
+	@FXML
+	ImageView inventoryFood2;
+	@FXML
+	ImageView inventoryFood3;
+	@FXML
+	ImageView inventoryFood4;
+	@FXML
+	ImageView inventoryImage1;
+	@FXML
+	ImageView inventoryImage2;
+	@FXML
+	ImageView inventoryImage3;
+	@FXML
+	ImageView inventoryImage4;
 	
     double imageX = 0;
     double imageY = 0;
 
 	public void initialise() {
+		List<ImageView> inventoryFoods = new ArrayList<ImageView>();
+		inventoryFoods.add(inventoryFood1);
+		inventoryFoods.add(inventoryFood2);
+		inventoryFoods.add(inventoryFood3);
+		inventoryFoods.add(inventoryFood4);
+		
+		List<ImageView> inventoryImages = new ArrayList<ImageView>();
+		inventoryImages.add(inventoryImage1);
+		inventoryImages.add(inventoryImage2);
+		inventoryImages.add(inventoryImage3);
+		inventoryImages.add(inventoryImage4);
+		
+		
 		List<Node> obstacles = new ArrayList<Node>();
 		obstacles.add(obstacle1);
 		obstacles.add(obstacle2);
@@ -56,6 +90,12 @@ public class Easy {
 		obstacles.add(obstacle8);
 		obstacles.add(obstacle9);
 		obstacles.add(obstacle10);
+		
+		List<ImageView> foods = new ArrayList<ImageView>();
+		foods.add(food1);
+		
+		List<ImageView> images = new ArrayList<ImageView>();
+		images.add(image1);
 		
 		imageX = character.getLayoutX();
 		imageY = character.getLayoutY();
@@ -70,6 +110,8 @@ public class Easy {
                 imageY -= 5;
             }
             updateImageView();
+            handleFoodCollision(character, foods, inventoryFoods);
+            handleImageCollision(character, images, inventoryImages);
         });
         
         character.setFocusTraversable(true);
@@ -87,7 +129,7 @@ public class Easy {
         character.setLayoutX(imageX);
         character.setLayoutY(imageY);
     	for (Node obstacle : obstacles) {
-    		if(obstacle != null && character.getBoundsInParent().intersects(obstacle.getBoundsInParent())) {
+    		if(character.getBoundsInParent().intersects(obstacle.getBoundsInParent())) {
     	        character.setLayoutX(oldImageX);
     	        character.setLayoutY(oldImageY);
 	            return true;
@@ -97,5 +139,37 @@ public class Easy {
         character.setLayoutX(oldImageX);
         character.setLayoutY(oldImageY);
     	return false;
+    }
+    
+    public void handleFoodCollision(ImageView character, List<ImageView> foods, List<ImageView> inventoryFoods)
+    {
+    	for (ImageView food : foods) {
+    		if(character.getBoundsInParent().intersects(food.getBoundsInParent()) && food.getScene() != null) {
+    			Integer number = Integer.parseInt(food.getId().substring(food.getId().length() - 1));
+	            AnchorPane sceneRoot = (AnchorPane)food.getScene().getRoot();
+	            sceneRoot.getChildren().remove(food);
+	            
+	            ImageView inventoryFood = inventoryFoods.get(number - 1);
+	            inventoryFood.setImage(food.getImage());
+	            
+	            break;
+	        }
+    	}
+    }
+    
+    public void handleImageCollision(ImageView character, List<ImageView> images, List<ImageView> inventoryImages)
+    {
+    	for (ImageView image : images) {
+    		if(character.getBoundsInParent().intersects(image.getBoundsInParent()) && image.getScene() != null) {
+    			Integer number = Integer.parseInt(image.getId().substring(image.getId().length() - 1));
+	            AnchorPane sceneRoot = (AnchorPane)image.getScene().getRoot();
+	            sceneRoot.getChildren().remove(image);
+	            
+	            ImageView inventoryImage = inventoryImages.get(number - 1);
+	            inventoryImage.setImage(image.getImage());
+	            
+	            break;
+	        }
+    	}
     }
 }
